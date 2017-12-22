0526b2460b7cabe7ee6ce180404a14cbe2b8a28c
Prateek Sharma <prateek13103718@gmail.com>
2017-12-23 00:30:12 +0530

    OnBoarding screen animation added

---
+++ OnboardingDemoLottie/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  OnboardingDemoLottie
+//
+//  Created by Prateek Sharma on 20/12/17.
+//  Copyright © 2017 Prateek Sharma. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet var scrollView : UIScrollView!
+    lazy var animationView : LOTAnimationView! = {
+        let view = LOTAnimationView(name: "walkthrough")
+        view.frame = CGRect(x: 0, y: 20, width: self.view.frame.size.width, height: 400)
+        view.clipsToBounds = false
+        view.contentMode = .scaleAspectFill
+        
+        view.loopAnimation = true
+    
+        return view
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(animationView)
+        
+        setupScrollView()
+    }
+    
+    func setupScrollView(){
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width * 6, height: self.scrollView.frame.size.height)
+        
+        scrollView.isPagingEnabled = true
+        
+        for i in 0...5 {
+            let label = UILabel()
+            label.text = "Page \(i)"
+            label.font = UIFont.systemFont(ofSize: 25, weight: UIFont.Weight.heavy)
+            label.sizeToFit()
+            label.center.y = scrollView.center.y
+            label.center.x = view.frame.size.width * CGFloat(i) + view.center.x
+            view.addSubview(label)
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let progress = scrollView.contentOffset.x / scrollView.contentSize.width
+        animationView.animationProgress = progress
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        animationView.play()
+    }
+    
+}
+
